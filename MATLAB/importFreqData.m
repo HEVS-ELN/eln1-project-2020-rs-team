11a45d73b1dd41094181667d627b15262dc35152
Rashit Rashiti <rashit_lol@hotmail.fr>
2020-03-26 09:30:25 +0100

    Rpr-Cpr Plot

--- MATLAB/importFreqData.m
+++ MATLAB/importFreqData.m
@@ -1,4 +1,4 @@
-path = 'C:\Users\alexxx\Desktop\Work\ELN1_Project2020\FilesForStudents\data\ConductiveProbe\';
+path = 'C:\Users\rashi\eln1-project-2020-rs-team\data\ConductiveProbe\';
 freq = [];
 V1=[];
 V2=[];
@@ -12,3 +12,19 @@ Amp2(:,k+1) = 10.^(data(:,3)/20);
 Phase2(:,k+1) = data(:,4)*pi/180;
 
 end
+
+A=Amp2;
+argH=Phase2;
+R1 = 10e3;
+w = freq*2*pi;
+
+
+Rpr=  (R1*A.*sqrt(1+(tan(argH)).^2))./(1- A.*sqrt(1+(tan(argH)).^2));
+Cpr= (tan(argH).*(R1+Rpr))./(w*R1.*Rpr);
+
+figure (1);
+semilogx(freq,20*log10(abs(Rpr)));
+legend('k0','k1','k2','k3','k4');
+figure (2);
+semilogx(freq,20*log10(abs(Cpr)));
+legend('k0','k1','k2','k3','k4');

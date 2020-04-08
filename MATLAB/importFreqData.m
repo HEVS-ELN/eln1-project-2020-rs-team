e43943e4bad8e4d0d2cdcb9125ff2105c1a1e264
Rashit Rashiti <rashit_lol@hotmail.fr>
2020-04-08 12:55:24 +0200

    Added ADC specs2

--- MATLAB/importFreqData.m
+++ MATLAB/importFreqData.m
@@ -62,3 +62,36 @@ title('Simulation Plot');
 xlabel('time[ms]');
 ylabel('voltage [V]');
 ylim([-0.3 0.3]);
+
+SampleFreq = 1e5; %the sample frequency for all the channels together
+                 %how does "scan mode work??"
+
+OutputSmpls = 2; %the number of output samples you will convert
+                  %How much RAM does the processor have? How much
+                  %of this can you use just for your signals?
+
+Vmax = 3;        %the maximum voltage we can convert with the ADC
+
+SignalFreq = 2e3;   %The frequency of your signal
+
+%data(:,1) is the time vector exported from LTspice
+%data(:,2) is one voltage channel measured
+%data(:,3) is another voltage channel measured
+
+%What does the following 5 lines do?
+
+t0 = data(1,1)+rand(1)/SignalFreq;%décalage de temps pour éviter une synchro et on commence plus tard que 0
+t1 = (t0:2/(SampleFreq):t0+(OutputSmpls-1)*2/(SampleFreq))';    %
+t2 = t1+1/SampleFreq;                                           %
+V1 = interp1(data(:,1), data(:,2), t1);                         %Valeur de y(t1)
+V2 = interp1(data(:,1), data(:,3), t2);                         %Valeur de y(t2)
+
+NoiseBits = 3;    %what are noise bits? Related to effective bits of the ADC
+
+%What do these lines do?? What is randn() ?
+V1dig=int16((2^15)*(V1/Vmax)+randn(OutputSmpls,1)*(2^NoiseBits));
+V2dig=int16((2^15)*(V2/Vmax)+randn(OutputSmpls,1)*(2^NoiseBits));
+
+%What do these lines do??
+V1d=double(V1dig)*Vmax/(2^15);
+V2d=double(V2dig)*Vmax/(2^15);
